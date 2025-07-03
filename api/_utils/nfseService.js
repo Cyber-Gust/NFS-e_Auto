eb9f31a4b8e3478a57b5002d473a040247ed5426
Cyber Gust <augusto.e.silva@hotmail.com>
2025-07-03 03:17:15 +0000

    xml

--- api/_utils/nfseService.js
+++ api/_utils/nfseService.js
@@ -65,9 +65,7 @@ function signXml(xml, tag) {
     "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
     "http://www.w3.org/2001/10/xml-exc-c14n#"
   ],
-  {
-    digestAlgorithm: "http://www.w3.org/2001/04/xmlenc#sha256"
-  }
+  "http://www.w3.org/2001/04/xmlenc#sha256"
 );
     sig.signingKey = privateKeyPem;
     sig.keyInfoProvider = {
